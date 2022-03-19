9e4133dd596673308bbae6e14e928c8ff29f62df
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-03-19 22:51:22 +0100

    Deploy to Azure

--- SimpleBankAPI/Startup.cs
+++ SimpleBankAPI/Startup.cs
@@ -100,9 +100,13 @@ namespace SimpleBankAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleBankAPI v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleBankAPI v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
